ea8006e75120faa4c9251963027daa18bfe1711c
alex <slipshot21@gmail.com>
2020-03-23 23:54:57 -0500

    deleted destructed experience props

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -2,22 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'react-moment'
 
-const ProfileExperience = ({experience : {company, title, location, current, to, from, description}}) => {
-    console.log('Company = ' + company)
+const ProfileExperience = ({experience }) => {
+    console.log(experience)
+    {/*company, title, location, current, to, from, description*/}
     return (
         <div>
-            <h3 className="text-dark">{company}</h3>
+            <h3 className="text-dark">{experience.company}</h3>
             <p>
-                <Moment format = 'YYYY/MM/DD'>{from}</Moment> - {!to ? ' Now' : <Moment format = 'YYYY/MM/DD'>{to}</Moment>}
+                <Moment format = 'YYYY/MM/DD'>{experience.from}</Moment> - {!experience.to ? ' Now' : <Moment format = 'YYYY/MM/DD'>{experience.to}</Moment>}
             </p>
             <p>
-                <strong>Location: </strong> {location}
+                <strong>Location: </strong> {experience.location}
             </p>
             <p>
-                <strong>Position: </strong> {title}
+                <strong>Position: </strong> {experience.title}
             </p>
             <p>
-                <strong>Description: </strong> {description}
+                <strong>Description: </strong> {experience.description}
             </p>
         </div>
     )
